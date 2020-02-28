ee61521a1430580f5922b704141d827e7a44d282
thejunglife <thejunglife@gmail.com>
2020-02-28 02:55:52 -0800

    rough draft of resume page

--- client/src/Pages/Resume/Resume.js
+++ client/src/Pages/Resume/Resume.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import resume from '../../assets/images/RESUME.pdf'
+// import { Document, Page, pdfjs } from 'react-pdf/dist/entry.parcel';
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 const Resume = () => {
+
+  // const istyle = {
+  //   height: "100%",
+  //   width: "100%"
+  // }
+  const divStyle ={
+    height: "800px",
+    width: "100%"
+  }
   
+  const [resumeState, setResumeState] = useState({
+    numPages: null,
+    pageNumber: 1
+  })
+
+  resumeState.onDocumentLoadSuccess = (numPages)  => {
+    setResumeState({ numPages })
+  }
+// temp fix 
   return(
-    <h1>Resume Page</h1>
+    <div>
+    <h1>Resume</h1>
+    <iframe src={resume} style={divStyle} />
+    </div>
   )
 }
 
 export default Resume
+      // <Document file={resume} onLoadSuccess={resumeState.onDocumentLoadSuccess}>
+      // <Page pageNumber={resumeState.pageNumber} />
+      // </Document>
